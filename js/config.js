3cd04539014b5d0f26e0a98a1d097fd145aaf7ec
boriskvashnin1-hash <boriskvashnin1@gmail.com>
2025-12-10 23:09:09 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Конфигурация Supabase
-const SUPABASE_URL = 'https://ваш-проект.supabase.co';
-const SUPABASE_ANON_KEY = 'ваш-ключ';
+const SUPABASE_URL = https://yhqchvmoymbfvumwgwse.supabase.co;
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlocWNodm1veW1iZnZ1bXdnd3NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzOTAyNzksImV4cCI6MjA4MDk2NjI3OX0.QRwyAqHGLPmEKYIp__3iqagewV_FEjoLFWyfy6cgeqo;
 
 // Инициализация Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
